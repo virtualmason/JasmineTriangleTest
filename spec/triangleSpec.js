aaa3a1e5734a6ce4dab2fbff67d19e1848db9158
Floyd <foo_32120@yahoo.com>
2016-06-03 19:20:31 -0400

    update algorithm to pass more test

--- spec/triangleSpec.js
+++ spec/triangleSpec.js
@@ -3,31 +3,20 @@ describe("Triangle",function(){
         
         expect(Triangle.numArgs(1,1,1)).toEqual(3);       
     });
-    
+    it("If sum of two sides don't equal 3rd return invalid triange", function(){
+        expect(Triangle.triTypes(1,2,3)).toEqual("invalid Triangle");
+    });
     it("3 equal sides should be equilateral", function(){        
         
         expect(Triangle.triTypes(1,1,1)).toEqual("equilateral");       
     });
     
     it("if only two sides are equal then it's isosceles", function(){
-        expect(Triangle.triTypes(1,1,4)).toEqual("isosceles");        
+        expect(Triangle.triTypes(2,2,3)).toEqual("isosceles");        
     });    
     
-    it("everything else should be scalene", function(){
-        expect(Triangle.triTypes(1,2,3)).toEqual("scalene");
-    });
+    
     
 });
     
 
-
-/*
-describe("Person", function() {
-    it("calls the getName() function", function() {
-        var testPerson = new Person();
-        spyOn(testPerson, "getName");
-        testPerson.toString();
-        expect(testPerson.getName).toHaveBeenCalled();
-    });
-});
-*/
--- src/triangle.js
+++ src/triangle.js
@@ -1,17 +1,23 @@
-  Triangle = {
-   triTypes : function(){   	
-       if (arguments[0]  === arguments[1] && arguments[1] ===   arguments[2]){
-       return "equilateral"; 
+    Triangle = {
+      triTypes : function(one, two, three){   	
+      if (one  === two && two === three){
+      return "equilateral"; 
            
-   } else if 
-       (arguments[0] ===arguments[1] || arguments[2] === arguments[1] ||  arguments[0] === arguments[2]) {
-       return "isosceles";
+   }  else if 
+       (one + two > three === false|| one + three >= two === false||three + two > one === false ) {
+        return "invalid Triangle"; 
 }
-   else return "scalene";       
-   },//Tritypes
-   numArgs : function(){
-   	return  arguments.length;
-   }
    
-
+       else if(one === two || one === three||  three === two  ) {
+       return "isosceles";
+       
+}      else if      (one !== two || one !== three  ||  three !== two  ) {
+       return "scalene";
+}
+        
+   },
+       numArgs : function(){
+       return  arguments.length;
+}
+   
 };
