8252fe9b9b9e4bfd3e6f1265ed0eaffd728a86a3
Floyd <foo_32120@yahoo.com>
2016-06-26 21:33:23 -0400

    fixed spacing

--- src/triangle.js
+++ src/triangle.js
@@ -1,14 +1,13 @@
 
     Triangle = {
-      triTypes : function (sideOne, sideTwo , sideThree){ 
+      triTypes : function (sideOne, sideTwo, sideThree) { 
           // make if statements clean
           var sidesEqual = sideOne === sideTwo && sideTwo  === sideThree && sideOne > 0;
           var  greaterThen0 = sideTwo > 0 && sideThree > 0;
           var  twoSidesGreaterThenthirdor = sideOne + sideTwo  > sideThree === false|| sideOne + sideThree > sideTwo  === false;
           var  twoSidesGreaterThenThirdNotEqualTo0 = sideThree + sideTwo  > sideOne === false|| sideOne === 0 || sideTwo  === 0 || sideThree === 0;
           
-          if (sidesEqual && greaterThen0)
-           {
+          if (sidesEqual && greaterThen0) {
               return "equilateral";          
           } 
           
