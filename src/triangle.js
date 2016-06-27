89d6f014ae32ad295ed6d0e778cf0476a31adb3b
Floyd <foo_32120@yahoo.com>
2016-06-26 20:40:25 -0400

    cleaning up if statments

--- src/triangle.js
+++ src/triangle.js
@@ -1,30 +1,34 @@
 
     Triangle = {
-      triTypes : function(sideOne, sideTwo , sideThree){   	
-      if (sideOne === sideTwo && sideTwo  === sideThree && sideOne > 0 
-         && sideTwo > 0 && sideThree > 0){
-      return "equilateral"; 
-           
-   }            
-          else if 
-       (sideOne + sideTwo  > sideThree === false|| sideOne + sideThree > sideTwo  === false||
-        sideThree + sideTwo  > sideOne === false|| sideOne === 0 || sideTwo  === 0 || sideThree === 0) {
-        return "Invalid Triangle"; 
-}
+      triTypes : function (sideOne, sideTwo , sideThree){ 
+          // make if statements clean
+          var sidesEqual = sideOne === sideTwo && sideTwo  === sideThree && sideOne > 0;
+          var  greaterThen0 = sideTwo > 0 && sideThree > 0;
+          var  twoSidesGreaterThenthirdor = sideOne + sideTwo  > sideThree === false|| sideOne + sideThree > sideTwo  === false;
+          var  twoSidesGreaterThenThirdNotEqualTo0 = sideThree + sideTwo  > sideOne === false|| sideOne === 0 || sideTwo  === 0 || sideThree === 0;
+          
+          if (sidesEqual && greaterThen0)
+           {
+              return "equilateral";          
+          } 
+          
+          else if (twoSidesGreaterThenthirdor || twoSidesGreaterThenThirdNotEqualTo0) {
+              return "Invalid Triangle"; 
+          }
    
-       else if(sideOne === sideTwo  || sideOne === sideThree||  sideThree === sideTwo   ) {
-       return "isosceles";
-       
-}      else if(sideOne !== sideTwo  || sideOne !== sideThree  ||  sideThree !== sideTwo   ) {
-       return "scalene";
-    
-}      
+          else if (sideOne === sideTwo  || sideOne === sideThree||  sideThree === sideTwo   ) {
+              return "isosceles";       
+          } 
+          
+          else if (sideOne !== sideTwo  || sideOne !== sideThree  ||  sideThree !== sideTwo   ) {
+             return "scalene";    
+          }      
       
         
-   }
+     }
       
    
-};
+   };
           
         
      
